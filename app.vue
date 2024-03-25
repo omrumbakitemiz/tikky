6e25de0337e6e02941e7b9e673f85be922784a37
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2024-03-25 22:18:23 +0300

    init

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <NuxtPwaManifest />
+    <NuxtLayout>
+      <div>
+        <h1>Nuxt Vite PWA</h1>
+        <ClientOnly> PWA Installed: {{ $pwa?.isPWAInstalled }} </ClientOnly>
+      </div>
+    </NuxtLayout>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="space-y-6 p-10 pb-16 block">
+    <div class="flex justify-between">
+      <div class="space-y-0.5">
+        <h2 class="text-2xl font-bold tracking-tight">Tikky 💸</h2>
+        <p class="text-muted-foreground">Pay Friends Easily: Your Bill Sharing Solution!</p>
+      </div>
+
+      <ThemeToggle />
+    </div>
+    <Separator class="my-6" />
+    <div class="flex flex-col space-y-8">
+      <div class="flex-1 lg:max-w-2xl">
+        <div class="space-y-6">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
