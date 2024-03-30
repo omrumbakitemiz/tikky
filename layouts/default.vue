479a7a3cdb94afa7453b2d46b95365425c060fbf
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2024-03-30 23:57:27 +0300

    chore(app): refactor

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,32 @@
+<script setup lang="ts">
+const config = useRuntimeConfig();
+</script>
+
 <template>
-  <div class="space-y-6 p-10 pb-16 block">
-    <div class="flex justify-between">
-      <div class="space-y-0.5">
-        <h2 class="text-2xl font-bold tracking-tight">Tikky 💸</h2>
-        <p class="text-muted-foreground">Pay Friends Easily: Your Bill Sharing Solution!</p>
+  <div class="space-y-6 p-10 pb-16 flex flex-col justify-between h-full">
+    <div>
+      <div class="flex justify-between">
+        <div class="space-y-0.5">
+          <h2 class="text-2xl font-bold tracking-tight">Tikky 💸</h2>
+          <p class="text-muted-foreground">Pay Friends Easily: Your Bill Sharing Solution!</p>
+        </div>
+
+        <ThemeToggle />
       </div>
 
-      <ThemeToggle />
-    </div>
-    <Separator class="my-6" />
-    <div class="flex flex-col space-y-8">
-      <div class="flex-1 lg:max-w-2xl">
-        <div class="space-y-6">
-          <slot />
+      <Separator class="my-6" />
+
+      <div class="flex flex-col space-y-8">
+        <div class="flex-1 lg:max-w-2xl">
+          <div class="space-y-6">
+            <slot />
+          </div>
         </div>
       </div>
     </div>
+
+    <footer class="">
+      <p class="text-sm">v{{ config.public.appVersion }}</p>
+    </footer>
   </div>
 </template>
