e7f3975cdc10464bfe4e55b470b7560547662216
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2024-03-31 20:59:22 +0300

    feat(vision): version 0.2.3

--- server/routes/vision.post.ts
+++ server/routes/vision.post.ts
@@ -1,7 +1,7 @@
 import OpenAI from 'openai';
 import { z } from 'zod';
 
-const openai = new OpenAI();
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 const visionSchema = z.object({
   imageString: z.string(),
